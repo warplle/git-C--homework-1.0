2d8861467b45991444fd67ec1be8f0a65ce3ad8a
adrian.rascovschi <adrian.rascovschi@MDMOB40507>
2022-04-18 15:07:33 +0300

    Add project files.

---
+++ C-Sharp-Lesson-1-Homework/Homework.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Lesson_1_Homework
+{
+    public class Homework
+    {
+        static public void Main(String[] args)
+        {
+            /* Exercise 1
+             * using "if" write a programm that checks whether an integer is greater then zero and even or odd
+             * Example: 
+             * |input   | result                               |
+             * |  1     | even                                 |
+             * |  2     | odd                                  |
+             * | -1     | the value should be greater then zero|
+             * 
+             */
+
+            /* Exercise 2
+            * using a ternary operator write a program that print if number divide to 4
+            * Example: 
+            * |input   | result                          |
+            * |  -4    | The number divide to 4          |
+            * |   2    | The number doesn't divide to 4  |
+            * 
+            */
+
+            /* Exercise 3
+             * using "switch" write a program that print the number of day of week
+             * Example: monday - 1, tuesday - 2 etc. 
+             * |day           |number                                  |
+             * |monday        |   1                                    |
+             * |tuesday       |   2                                    |
+             * |wednesday     |   3                                    |
+             * |thursday      |   4                                    |
+             * |friday        |   5                                    |
+             * |saturday      |   6                                    |
+             * |sunday        |   7                                    |
+             * |default value |Wrong value! Please give a day of a week|
+             */
+
+            /* Exercise 4
+             * write a program that will print input character is a vowel, consonant or not a letter
+             * Method 1: using a switch case
+             * Method 2: using if
+             * |input| result            |
+             * |  a  | a is a vowel      |
+             * |  b  | b is a consonant  |
+             * |  5  | 5 is not s letter |
+             */
+        }
+    }
+}
