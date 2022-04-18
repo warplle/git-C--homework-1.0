4440232c782425e7029bfbe2982379ce4781b202
6432633 <58008289+6432633@users.noreply.github.com>
2022-04-18 17:25:58 +0300

    Update Homework.cs

--- C-Sharp-Lesson-1-Homework/Homework.cs
+++ C-Sharp-Lesson-1-Homework/Homework.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,31 +8,47 @@ namespace C_Sharp_Lesson_1_Homework
 {
     public class Homework
     {
-        static public void Main(String[] args)
+        public void CheckIfNumberIsEvenOrOdd(int number)
         {
             /* Exercise 1
-             * using "if" write a programm that checks whether an integer is greater then zero and even or odd
-             * Example: 
-             * |input   | result                               |
-             * |  1     | even                                 |
-             * |  2     | odd                                  |
-             * | -1     | the value should be greater then zero|
-             * 
-             */
-
+          * using "if" write a programm that checks whether an integer is greater then zero and even or odd
+          * Example:
+          * -------------------------------------------------
+          * |input   | result                               |
+          * |--------|--------------------------------------|
+          * |  1     | even                                 |
+          * |--------|--------------------------------------|
+          * |  2     | odd                                  |
+          * |--------|--------------------------------------|
+          * | -1     | the value should be greater then zero|
+          * -------------------------------------------------
+          * 
+          */
+            //your code here
+        }
+        public void NumberDivideToFour(int number)
+        {
             /* Exercise 2
-            * using a ternary operator write a program that print if number divide to 4
-            * Example: 
-            * |input   | result                          |
-            * |  -4    | The number divide to 4          |
-            * |   2    | The number doesn't divide to 4  |
-            * 
-            */
-
+        * using a ternary operator write a program that print if number divide to 4
+        * Example: 
+        * --------------------------------------------
+        * |input   | result                          |
+        * |--------|---------------------------------|
+        * |  -4    | The number divide to 4          |
+        * |--------|---------------------------------|
+        * |   2    | The number doesn't divide to 4  |
+        * --------------------------------------------
+        */
+            //your code here
+        }
+        public void DayOfWeek(string day)
+        {
             /* Exercise 3
-             * using "switch" write a program that print the number of day of week
+             * using "switch" statement write a program that print the number of day of week
              * Example: monday - 1, tuesday - 2 etc. 
+             * ---------------------------------------------------------
              * |day           |number                                  |
+             * |--------------|----------------------------------------|
              * |monday        |   1                                    |
              * |tuesday       |   2                                    |
              * |wednesday     |   3                                    |
@@ -41,17 +57,48 @@ namespace C_Sharp_Lesson_1_Homework
              * |saturday      |   6                                    |
              * |sunday        |   7                                    |
              * |default value |Wrong value! Please give a day of a week|
+             * ---------------------------------------------------------
              */
-
+            
+            switch (day)
+            {
+                //your code here
+            }
+        }
+        public void CheckLetterIfVowel(char character)
+        {
             /* Exercise 4
              * write a program that will print input character is a vowel, consonant or not a letter
              * Method 1: using a switch case
              * Method 2: using if
+             * ---------------------------
              * |input| result            |
+             * |-----|-------------------|
              * |  a  | a is a vowel      |
              * |  b  | b is a consonant  |
-             * |  5  | 5 is not s letter |
+             * ---------------------------
              */
+            //your code here
+
+        }
+        public static void Main(String[] args)
+        {
+         Homework homework = new Homework();
+
+            homework.CheckIfNumberIsEvenOrOdd(-11);
+            homework.CheckIfNumberIsEvenOrOdd(11);
+            homework.CheckIfNumberIsEvenOrOdd(8);
+            //---------------------------------------
+            homework.NumberDivideToFour(10);
+            homework.NumberDivideToFour(16);
+            //---------------------------------------
+            homework.DayOfWeek("Monday");
+            homework.DayOfWeek("Sunday");
+            homework.DayOfWeek("some day");
+            //---------------------------------------
+            homework.CheckLetterIfVowel('p');
+            homework.CheckLetterIfVowel('i');
+
         }
     }
 }
